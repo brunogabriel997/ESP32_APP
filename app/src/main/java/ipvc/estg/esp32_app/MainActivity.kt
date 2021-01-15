9874226c37dee2953ebe302e17ac2c481344a5fe
brunogabriel997 <brunogabriel997@gmail.com>
2021-01-15 14:34:51 +0000

    Graphics Updated
    Obtem os valores de um dia estatico e coloca-os no grafico

--- app/src/main/java/ipvc/estg/esp32_app/MainActivity.kt
+++ app/src/main/java/ipvc/estg/esp32_app/MainActivity.kt
@@ -8,6 +8,10 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -25,8 +29,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_incial)
-        val barChart = findViewById<View>(R.id.barchart) as BarChart
+        setContentView(R.layout.activity_main)
+        //val barChart = findViewById<View>(R.id.barchart) as BarChart
 
         // data atual
         val formataData_ano = SimpleDateFormat("yyyy")
@@ -41,29 +45,22 @@ class MainActivity : AppCompatActivity() {
         val dataFormatada_mes = formataData_mes.format(data)
         val dataFormatada_ano = formataData_ano.format(data)
 
+
+
 /*
+        val num = 5
+
         val entries: ArrayList<BarEntry> = ArrayList()
-        entries.add(BarEntry(8f, 0))
-        entries.add(BarEntry(2f, 1))
-        entries.add(BarEntry(5f, 2))
-        entries.add(BarEntry(20f, 3))
-        entries.add(BarEntry(15f, 4))
-        entries.add(BarEntry(19f, 5))
-
-
-
-
-
-        val bardataset = BarDataSet(entries, "Cells")
-
         val labels = ArrayList<String>()
-        labels.add("2016")
-        labels.add("2015")
-        labels.add("2014")
-        labels.add("2013")
-        labels.add("2012")
-        labels.add("2011")
+        val bardataset = BarDataSet(entries, "Cells")
+        for (i in 6 downTo 0) {
+            println(i)
 
+            entries.add(BarEntry(num.toFloat(), i))
+
+            labels.add(i.toString())
+
+        }
         val data1 = BarData(labels, bardataset)
         barChart.data = data1 // set the data and list of labels into chart
 
@@ -74,21 +71,16 @@ class MainActivity : AppCompatActivity() {
 */
 
 
-        for (i in 23 downTo 0) {
-            println(i)
-
-        }
-
-
         val button_atual = findViewById<Button>(R.id.botao)
         button_atual.setOnClickListener {
             val dataFormatada = formataData_mes.format(data)
             //System.out.println("Data formatada " + dataFormatada );
             //Toast.makeText(this@MainActivity, "Data formatada " + dataFormatada_ano, Toast.LENGTH_LONG).show()
-            basicReadWrite(dataFormatada_hora, dataFormatada_dia.toInt(), dataFormatada_ano.toInt())
+
 
 
         }
+        basicReadWrite(dataFormatada_hora, dataFormatada_dia.toInt(), dataFormatada_ano.toInt())
 
 
     }
@@ -98,6 +90,8 @@ class MainActivity : AppCompatActivity() {
         // [START write_message]
         // Write a message to the database
         val myRef = FirebaseDatabase.getInstance().getReference()
+        val barChart = findViewById<View>(R.id.barchart) as BarChart
+
 
         // [START read_message]
         // Read from the database
@@ -105,15 +99,46 @@ class MainActivity : AppCompatActivity() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+                /*
                 val value =
-                    dataSnapshot.child("ESP32_Version1/"+ dataFormatada_ano +"/janeiro/dia_"+dataFormatada_dia+"/" + dataFormatada_hora + "h").value.toString()
+                        dataSnapshot.child("ESP32_Version1/"+ dataFormatada_ano +"/janeiro/dia_"+dataFormatada_dia+"/" + dataFormatada_hora + "h").value.toString()
+                */
 
-                val value2 =
-                    dataSnapshot.child("ESP32_Version1/"+ dataFormatada_ano +"/janeiro/dia_"+dataFormatada_dia+"/18h").value.toString()
 
-                Log.d(TAG, "Value1 is: $value")
-                Log.d(TAG, "Value2 is: $value2")
-                Toast.makeText(this@MainActivity, value, Toast.LENGTH_SHORT).show()
+                var value2 = 0
+                val entries: ArrayList<BarEntry> = ArrayList()
+                val labels = ArrayList<String>()
+                val bardataset = BarDataSet(entries, "Cells")
+                for (i in 23 downTo 0) {
+                    val value =
+                            dataSnapshot.child("ESP32_Version1/"+ dataFormatada_ano +"/janeiro/dia_7/" + i + "h").value.toString()
+
+                    if(value == "null") {
+                        value2 = 0
+                    }
+                    else {
+                        value2 = value.toInt()
+                    }
+                    entries.add(BarEntry(value2.toFloat(), i))
+
+                    labels.add(i.toString())
+
+                }
+                val data1 = BarData(labels, bardataset)
+                barChart.data = data1 // set the data and list of labels into chart
+
+                barChart.setDescription("Set Bar Chart Description Here") // set the description
+
+                bardataset.setColors(ColorTemplate.COLORFUL_COLORS)
+                barChart.animateY(3000)
+
+
+
+
+
+
+                //Log.d(TAG, "Value1 is: $value")
+                //Toast.makeText(this@MainActivity, value, Toast.LENGTH_SHORT).show()
             }
 
             override fun onCancelled(error: DatabaseError) {
